d7139986a81a9fda4cd940a4cd1d27457cc58299
annabush092 <annabush092@gmail.com>
2017-10-31 13:51:02 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 // Code Goes Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Order from './Order';
+
+ReactDOM.render(
+  < Order cone={false} size="medium" scoops={["chocolate", "vanilla"]} orderInfo={{customerName: "Anna", orderedAt: 577}} />,
+  document.getElementById('root'))
